87c55c8789ec66bb9fe75e8b4329e39c7f27a1c5
FreemanLuka <freemanluka@gmail.com>
2024-04-12 17:29:45 +0100

    My third commit; adding my definition of life

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ console.log("The sum of " + number1 + " and " + number2 + " is " + sum)
 }
 
 sumTwoNumbers(5, 22)
+
+console.log("The whole earth is my scope! I am not ignorant of my duty! I am here for God; to always make the earth better!")
